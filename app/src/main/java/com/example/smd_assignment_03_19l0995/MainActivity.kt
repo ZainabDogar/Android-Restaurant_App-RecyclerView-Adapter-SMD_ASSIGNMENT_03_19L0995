5d3106c1d8fbf25eb0a54ccce152a428df456b06
ZAINAB DOGAR <l190995@lhr.nu.edu.pk>
2022-04-17 20:11:37 +0500

    First block

---
+++ app/src/main/java/com/example/smd_assignment_03_19l0995/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.smd_assignment_03_19l0995
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
