1e9d3e61e1e49af00ad23fe3e31a003319e01fb7
ZAINAB DOGAR <l190995@lhr.nu.edu.pk>
2022-04-21 12:50:42 +0500

    Add page

--- app/src/main/java/com/example/smd_assignment_03_19l0995/MainActivity.kt
+++ app/src/main/java/com/example/smd_assignment_03_19l0995/MainActivity.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
